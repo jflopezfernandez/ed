45833d86a923038eb8412b333d1e4fed2991c7c5
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-09-02 15:42:10 -0400

    Replicated initial ed behavior

--- src/main.c
+++ src/main.c
@@ -1,14 +1,36 @@
 
 #include "ed.h"
 
-int main(int __attribute__((unused)) argc, const char __attribute__((unused)) *argv[])
-{
-    static const char* prompt = " > ";
+static const char* prompt = "";
 
-    char* input_buffer = NULL;
+static char* input_buffer = NULL;
+
+int main(int argc, char **argv)
+{
+    if (argc == 1) {
+        fprintf(stderr, "%s\n", "No filename given");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE* file = fopen(argv[1], "r+");
+
+    if (file == NULL) {
+        fprintf(stderr, "[Error] %s\n", "Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t characters = 0;
+
+    int current_character = 0;
+
+    while ((current_character = fgetc(file)) != EOF) {
+        ++characters;
+    }
+
+    printf("%lu\n", characters);
 
     while ((input_buffer = readline(prompt))) {
-        if (strcmp(input_buffer, "quit") == 0) {
+        if (strcmp(input_buffer, "q") == 0) {
             break;
         }
 
@@ -16,6 +38,8 @@ int main(int __attribute__((unused)) argc, const char __attribute__((unused)) *a
 
         free(input_buffer);
     }
+
+    fclose(file);
 
     return EXIT_SUCCESS;
 }
